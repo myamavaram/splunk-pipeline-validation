d1754b8a9a0a481a73f061f513e317a58468f0e4
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-22 16:57:50 +0000

    remove_incr_version.sh edited online with Bitbucket

--- remove_incr_version.sh
+++ remove_incr_version.sh
@@ -4,12 +4,8 @@
 FILE="DA-ESS-security-content-10-dev/default/app.conf"
 
 # Remove the [install] section
-if grep -q '\[install\]' "$FILE"; then
-  sed -i '/\[install\]/,/^\[/d' "$FILE"
-  echo "Removed Install section"
-else
-  echo "install section doesn't exist"
-fi
+#sed -i '/\[install\]/,/^\[/d' "$FILE"
+sed -i '/^\[install\]/,/^\[/{/^\[ui\]/!d}' "$FILE"
 
 # Extract the current version
 current_version=$(grep -Po '(?<=version = )[\d.]*' "$FILE")
@@ -20,7 +16,7 @@ if [[ $current_version =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
     minor=${BASH_REMATCH[2]}
     patch=${BASH_REMATCH[3]}
     new_version="$major.$minor.$((patch + 1))"
-
+    
     # Update the version number in the file
     sed -i "s/version = $current_version/version = $new_version/" "$FILE"
     echo "Version updated from $current_version to $new_version"
