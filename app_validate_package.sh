e4ace6eb1c6ddee1a174feb88036bb7b289925c8
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-22 14:55:37 +0000

    app_validate_package.sh edited online with Bitbucket

--- app_validate_package.sh
+++ app_validate_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo cd /opt/splunk/var/run/splunk/deploy/apps
+#sudo cd /opt/splunk/var/run/splunk/deploy/apps
+set -e
 echo $PWD
 echo "remove .bundle file from  /opt/splunk/var/run/splunk/deploy/apps"
 sudo rm -f /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-*.bundle
@@ -7,6 +8,6 @@ sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
 echo "check the files in package"
 sudo ls -l DA-ESS-security-content-10-dev
 sudo ls -l DA-ESS-security-content-10-dev/default
-sudo slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
-sudo slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
-sudo ls -l
+sudo /opt/splunk/var/run/splunk/deploy/apps/slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+sudo /opt/splunk/var/run/splunk/deploy/apps/slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
