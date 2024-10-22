b228f9776d07f17cb64953226a29408351193e3d
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-22 14:57:35 +0000

    app_validate_package.sh edited online with Bitbucket

--- app_validate_package.sh
+++ app_validate_package.sh
@@ -8,6 +8,9 @@ sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
 echo "check the files in package"
 sudo ls -l DA-ESS-security-content-10-dev
 sudo ls -l DA-ESS-security-content-10-dev/default
-sudo /opt/splunk/var/run/splunk/deploy/apps/slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
-sudo /opt/splunk/var/run/splunk/deploy/apps/slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+sudo chmod -R 757 opt/splunk/var/run/splunk/deploy
+sudo chmod -R 775 opt/splunk/var/run/splunk/deploy/apps
+cd /opt/splunk/var/run/splunk/deploy/apps
+sudo slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+sudo slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
 sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
