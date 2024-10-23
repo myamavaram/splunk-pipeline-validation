8a4c2d49f26a6130872d9f3fc3b592ae405b8afe
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-23 04:03:10 +0000

    stack_config_acs_login.sh edited online with Bitbucket

--- stack_config_acs_login.sh
+++ stack_config_acs_login.sh
@@ -1,6 +1,6 @@
 acs config add-stack transunion-dev --target-sh sh-i-0c5c6de335a7eccb0
 acs config use-stack transunion-dev --target-sh sh-i-0c5c6de335a7eccb0
-.dev_es.sh
+./dev_es.sh
 acs login
 app_package=$(/usr/bin/find /opt/splunk/var/run/splunk/deploy/apps -type f -name 'DA-ESS*' -print0 | xargs -0 ls -1t | head -n 1 | xargs -n 1 basename)
 echo $app_package
