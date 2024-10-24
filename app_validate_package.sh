756b04f8c4ce34b74bd046ee03bd87ce08649788
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-24 09:11:26 +0000

    app_validate_package.sh edited online with Bitbucket

--- app_validate_package.sh
+++ app_validate_package.sh
@@ -5,13 +5,13 @@ set -e
 #sudo chmod -R 775 /opt/splunk/var/run/splunk/deploy/apps
 #cd /opt/splunk/var/run/splunk/deploy/apps
 #echo $PWD
-sudo rm -f /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-*.bundle
-sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
-echo "check the files in package"
-sudo ls -l DA-ESS-security-content-10-dev
-sudo ls -l DA-ESS-security-content-10-dev/default
-sudo slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
-sudo slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev -o /opt/splunk/var/run/splunk/deploy/apps
-sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
-echo "running splunk-appinspect"
-sudo splunk-appinspect inspect /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev-1.0.1.tar.gz --included-tags cloud --included-tags self-service --excluded-tags splunk-app
+rm -f /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-*.bundle
+#sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
+echo "running splim validation command"
+#sudo ls -l DA-ESS-security-content-10-dev
+#sudo ls -l DA-ESS-security-content-10-dev/default
+slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+#sudo slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev -o /opt/splunk/var/run/splunk/deploy/apps
+#sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
+#echo "running splunk-appinspect"
+#sudo splunk-appinspect inspect /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev-1.0.1.tar.gz --included-tags cloud --included-tags self-service --excluded-tags splunk-app
