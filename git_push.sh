91b5b3f2494189fa57ab220a5a2c7e53829bbe89
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-24 11:22:53 +0000

    git_push.sh created online with Bitbucket

---
+++ git_push.sh
@@ -0,0 +1,5 @@
+git init
+git checkout -b feature/splunk-app-package-${BUILD_NUMBER}
+git add .
+git commit -m "Automatic changes commiting"
+git push https://myamavaram@bitbucket.org/harness-test-repo/splunk-cloud-pipeline.git feature/splunk-app-package-${BUILD_NUMBER}
