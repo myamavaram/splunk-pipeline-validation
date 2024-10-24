46a707c772bcd77572f7086a958bc0765571ba6a
Manitheja Yamavaram <myamavaram@softility.com>
2024-10-24 11:55:57 +0000

    app_validate_package.sh edited online with Bitbucket

--- app_validate_package.sh
+++ app_validate_package.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 #sudo cd /opt/splunk/var/run/splunk/deploy/apps
 set -e
-#sudo chmod -R 757 /opt/splunk/var/run/splunk/deploy
-#sudo chmod -R 775 /opt/splunk/var/run/splunk/deploy/apps
-#cd /opt/splunk/var/run/splunk/deploy/apps
-#echo $PWD
 rm -f /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-*.bundle
-#sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
 echo "running splim validation command"
-#sudo ls -l DA-ESS-security-content-10-dev
-#sudo ls -l DA-ESS-security-content-10-dev/default
 slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
-#sudo slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev -o /opt/splunk/var/run/splunk/deploy/apps
+slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev -o /opt/splunk/var/run/splunk/deploy/apps
+echo "running splunk-appinspect"
+sudo splunk-appinspect inspect /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev-1.0.1.tar.gz --included-tags cloud --included-tags self-service --excluded-tags splunk-app
+
+#sudo chmod -R 757 /opt/splunk/var/run/splunk/deploy
+
+#sudo chmod -R 775 /opt/splunk/var/run/splunk/deploy/apps
+
+#cd /opt/splunk/var/run/splunk/deploy/apps
+
+#echo $PWD
+
+#rm -f /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-*.bundle
+
 #sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
+
+#echo "running splim validation command"
+
+#sudo ls -l DA-ESS-security-content-10-dev
+
+#sudo ls -l DA-ESS-security-content-10-dev/default 
+
+#slim validate /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev
+
+#slim package /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev -o /opt/splunk/var/run/splunk/deploy/apps
+
+#sudo ls -l /opt/splunk/var/run/splunk/deploy/apps
+
 #echo "running splunk-appinspect"
+
 #sudo splunk-appinspect inspect /opt/splunk/var/run/splunk/deploy/apps/DA-ESS-security-content-10-dev-1.0.1.tar.gz --included-tags cloud --included-tags self-service --excluded-tags splunk-app
