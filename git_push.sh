89ee4cd3e79612fdbcb244d3cbbc97805e309614
myamavaram <myamavaram@softility.com>
2024-10-28 16:28:22 +0000

    changes

--- git_push.sh
+++
@@ -1,6 +0,0 @@
-echo "git commands"
-git init
-git checkout -b feature/splunk-app-package-${BUILD_NUMBER}
-git add .
-git commit -m "Automatic changes commiting"
-git push https://myamavaram@bitbucket.org/harness-test-repo/splunk-cloud-pipeline.git feature/splunk-app-package-${BUILD_NUMBER}
